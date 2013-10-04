0f4bfa3b09e1bd3f4eff8d1963ac54a80f466eec
pdiemert <pete_diemert@msn.com>
2013-10-03 17:08:33 -0700

    working

---
+++ bin/cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+;(function ()
+{ // wrapper in case we're in module_context mode
+
+	// windows: running "graphdat-relay blah" in this folder will invoke WSH, not node.
+	if (typeof WScript !== "undefined")
+	{
+		WScript.echo("graphdat-relay does not work when run\n" + "with the Windows Scripting Host\n\n" + "'cd' to a different directory,\n" + "or type 'graphdat-relay.cmd <args>',\n" + "or type 'node graphdat-relay <args>'.");
+		WScript.quit(1);
+		return;
+	}
+
+	require('../lib/relay.js');
+})();
